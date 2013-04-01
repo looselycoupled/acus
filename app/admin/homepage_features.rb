9e53cddf3d91072af05bbc8cd532b30360e72e12
Allen Leis <allen.leis@threespot.com>
2013-04-01 10:29:39 -0400

    adds homepage features model; modifies layout to 1000px max width and pilled masthead

---
+++ app/admin/homepage_features.rb
@@ -0,0 +1,13 @@
+ActiveAdmin.register HomepageFeature do
+
+
+  index do
+    column :title
+    column :super_title
+    column :updated_at
+    default_actions
+  end
+
+
+
+end
---
+++ app/models/homepage_feature.rb
@@ -0,0 +1,3 @@
+class HomepageFeature < ActiveRecord::Base
+  attr_accessible :content, :link, :link_text, :super_title, :title
+end
