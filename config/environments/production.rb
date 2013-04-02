676c3f79cab246013961512a208b561797e644f8
Allen Leis <allen.leis@threespot.com>
2013-04-02 10:59:19 -0400

    adds paperclip config to prod env file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -100,5 +100,21 @@ Shortline::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  
+  # give working path to Paperclip
+  Paperclip.options[:command_path] = "/usr/local/bin/"
+
+  # define paperclip config defaults
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['S3_BUCKET_NAME'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    },
+    :s3_protocol => ''
+  }
+
+  
 
 end
