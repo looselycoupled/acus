511a8bb15435a7b2525eb7fed672381c23868969
Allen Leis <allen.leis@threespot.com>
2013-05-25 02:37:06 -0400

    adds random photo to resource seed file

--- db/fixtures/resources.rb
+++ db/fixtures/resources.rb
@@ -1,8 +1,10 @@
+require 'open-uri'
 
-15.times do
+20.times do
   Resource.seed(:title) do |r|
     r.title = Faker::Company::catch_phrase
     r.teaser = Faker::Lorem::paragraph(rand(3..5))
     r.content = Faker::Lorem::paragraphs(rand(2..4)).map{|p| "<p>#{p}</p>"}.join
+    r.photo = OpenURI.open_uri("http://lorempixel.com/200/200/abstract/")
   end
 end
